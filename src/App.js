4bd705a00155629865d6f4db4a479106984e936d
Diego Jarauta <diegojarauta8@gmail.com>
2020-12-09 17:54:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+
+function Cartas(props) {
+    
+  let [cartas,setCartas] = useState([])
+    useEffect(function () {
+        fetch(`https://api.magicthegathering.io/v1/cards?set=${props.code}`).then(respuesta =>respuesta.json()).then(datos => {
+            setCartas(datos.cards)
+        })
+    }, [props.code])
+
+   
+
+    let mostrarCartas = cartas.map(carta=>{
+        return (<div>
+            <h1>{carta.name}</h1>
+            <p>{carta.text}</p>
+            <img className="carta" src={carta.imageUrl} width="100"/>
+        </div>)
+    })
+    
+    return mostrarCartas
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    let [data,setData] = useState([])
+    let [codeSet, setCodeSet] = useState("")
+
+    useEffect(function () {
+        fetch("https://api.magicthegathering.io/v1/sets").then(respuesta =>respuesta.json()).then(datos => {
+       setData(datos.sets)
+        })
+    }, [])
+
+    let mostrarSets = data.map(set=>{
+        return <option value={set.code}>{set.name}</option>
+    })
+
+    function seleccion(e) {
+        setCodeSet(e.target.value)
+    }
+
+    return (<>
+    <select onChange={seleccion}>
+        <option>--</option>
+        {mostrarSets}
+    </select>
+    <div>
+        <Cartas code={codeSet}/>
     </div>
-  );
+    </>)
+    
 }
 
 export default App;
